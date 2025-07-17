f14a69a6372b80ec84b0f1db40084ec34856f91a
Iskander-rzk <sasha1koval@gmail.com>
2025-07-17 18:59:15 +0700

    release 2

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ import uvicorn
 from fastapi import FastAPI
 from pathlib import Path
 from app.api.router import router as api_router
+from app.api.router import router
+
+app = FastAPI()
+app.include_router(router)
 
 BASE_DIR = Path(__file__).resolve().parent
 ENV_FILE = BASE_DIR / ".env"
@@ -9,7 +13,14 @@ ENV_FILE = BASE_DIR / ".env"
 app = FastAPI()
 app.include_router(api_router)
 
-if __name__ == "__main__":
+def run():
     uvicorn.run(
-        "main:app", reload=True, env_file=ENV_FILE
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        env_file=ENV_FILE
     )
+
+if __name__ == "__main__":
+    run()
